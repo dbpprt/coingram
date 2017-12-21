df59860549191c1e1699da22333f03871b17ad21
Dennis Bappert <bappert@outlook.com>
2017-12-21 19:53:33 +0100

    added empty project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoinGram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
